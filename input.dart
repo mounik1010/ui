a8562fcb7167fd1034c4b0a66e89ff83e6958ef2
mounik1010 <mounikmudhalkar1010@gmail.com>
2025-11-15 08:02:28 +0530

    Update input.dart

--- input.dart
+++ input.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: MyForm()));
-
-class MyForm extends StatelessWidget {
-  @override
-  Widget build(BuildContext c) => Scaffold(
-    body: Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          TextField(),
-          TextField(),
-          ElevatedButton(onPressed: (){}, child: Text("Submit"))
-        ],
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              Text("Name:"),
+              TextField(),
+              Text("Roll.no"),
+              TextField(),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Submit"),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
